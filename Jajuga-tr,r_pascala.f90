765b225b77d02e4fd902122fb2cf44eae1c54398
Kacper Jajuga <kacper.jajuga123@gmail.com>
2022-06-08 00:08:06 +0200

    Add created earlier code and files

---
+++ Jajuga-tr,r_pascala.f90
@@ -0,0 +1,34 @@
+PROGRAM Jajuga_tr_pascala
+
+    !Kacper Jajuga, grupa: D2, numer indeksu: 150678, kierunek: informatyka (PUW), rok I, semestr II
+
+  INTEGER n, f, i, j, k
+  
+  OPEN(10,file='Jajuga-tr,r_pascala.txt')
+  
+  WRITE(*,*) 'Program wyslietla pierwsze 15 wierszy Trojkata Pascala'
+  
+  n = 14 !zmienna pomocna przy wyswietlaniu trojkata
+
+  DO i = 0, n
+    f=1 !poczatek trojkata
+    DO j = 1, n - i
+      WRITE(*,'(A)',advance='NO') '    ' !wyswietlanie pustych miejsc, ktore stworza trojkat
+      WRITE(10,'(A)',advance='NO') '    '
+    END DO !j
+    DO k = 0, i
+      WRITE(*,'(I8)',advance='NO') f !wyswietlanie kolejnych liczb w trojkacie
+      WRITE(10,'(I8)',advance='NO') f
+      f = f * (i - k)/(k + 1) !obliczanie kolejnych liczb w trojkacie
+    END DO !k
+    WRITE(*,*) !oddzielanie kolejnych wierszy
+    wRITE(*,*) !niewymagana instrukcja, napisalem tylko dla celow estetycznych
+    WRITE(10,*)
+    WRITE(10,*)
+  END DO !i
+  
+  WRITE(*,*) 'Aby zakonczyc program nacisnij ENTER'
+  READ(*,*)
+  CLOSE(10)
+  STOP
+END PROGRAM Jajuga_tr_pascala
